e04952ebf472b66ab0a6ba19bb55fca032f7d547
Laurence Mike Santos <114161105+lmsantos15@users.noreply.github.com>
2022-10-12 20:37:17 +0800

    Checkpoint

---
+++ week5/checkpt.py
@@ -0,0 +1,28 @@
+# 05 Prepare: Checkpoint
+print()
+
+# COMPARING NUMBERS
+first_number = int(input("What is the first number? "))
+second_number = int(input("What is the second number? "))
+if first_number > second_number:
+    print("The first number is greater.")
+else:
+        print("The first number is not greater.")
+
+if first_number == second_number:
+    print("The numbers are equal")
+else:
+       print("The number are not equal.") 
+
+if second_number > first_number:
+    print("The second number is greater.")
+else:
+        print("The second number is not greater")
+print()
+
+animalz = input("What is your favorite animal? ")
+if animalz.lower == "dog":
+    print("That one if my favorite!")
+else:
+    print("Cool, my favorite animal is dog.")
+print()
