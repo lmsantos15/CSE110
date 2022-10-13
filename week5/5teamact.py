6d6e8be898c9c1eca801bbb0c9184c745203693a
Laurence Mike Santos <114161105+lmsantos15@users.noreply.github.com>
2022-10-13 22:29:00 +0800

    Pending Team Activity

---
+++ week5/5teamact.py
@@ -0,0 +1,24 @@
+# 05 Teach: Team Activity
+# Grade Calculator
+
+"""
+A >= 90
+B >= 80
+C >= 70
+D >= 60
+F < 60
+"""
+
+# Empty String - To initialize an empty string in Python, 
+# Just create a variable and don't assign any character or even no space. 
+# This means assigning “” to a variable to initialize an empty string.
+comment = ""
+
+grade = int(input("Enter you grade here: "))
+
+sign = grade % 10
+
+
+
+
+
