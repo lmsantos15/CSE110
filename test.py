b99de5938c92a566847048856cfd5057549f6e95
Laurence Mike Santos <114161105+lmsantos15@users.noreply.github.com>
2022-10-18 21:45:07 +0800

    Boolean

--- test.py
+++ test.py
@@ -1,39 +1,13 @@
-grade = int(input("What is your grade percent? "))
+x = 6
+y = 6
 
-if grade >= 90:
-    letter = "A"
-elif grade >= 80:
-    letter = "B"
-elif grade >= 70:
-    letter = "C"
-elif grade >= 60:
-    letter = "D"
+if x == 5:
+    print("a")
+
+    if y == 6:
+        print("b")
 else:
-    letter = "F"
+    print("c")
 
-# Stretch Challenge 1
-sign = ""
-
-last_digit = grade % 10
-
-if last_digit >= 7:
-    sign = "+"
-elif last_digit < 3:
-    sign = "-"
-else:
-    sign = ""
-
-# Stretch Challenge 2
-if grade >= 93:
-    sign = ""
-
-# Stretch Challenge 3
-if letter == "F":
-    sign = ""
-
-print(f"Your letter grade is: {letter}{sign}")
-
-if grade >= 70:
-    print("Congratulations! You passed the class!")
-else:
-    print("Stay focused and you'll get it next time!")
+    if y == 10:
+        print("d")
---
+++ week6/prepmat.py
@@ -0,0 +1,45 @@
+# # #06 Prepare: Preparation Material
+
+# # gpa = 85
+# # lowest_grade = 70
+
+# # if gpa >= .85:
+# #     if lowest_grade >= .70:
+# #         print("Well Done!")
+
+# # # Another example
+
+# # if gpa >= .85 and lowest_grade >= .70:
+# #     print("Well Done!")
+
+
+# # # or
+
+# # if gpa >= .85 and lowest_grade >= .70:
+# #     honour_roll = True
+
+# # else:
+# #     honour_roll = False
+
+# # # Somewhere later in the code
+
+# # if honour_roll:
+# #     print("Well Done!")
+
+# print()
+# # A student makes honour roll if their average grade is >= 85
+# # and their lowest grade is not below 70
+# gpa = float(input("What was your Grade Point Average? "))
+# lowest_grade = float(input("What was your lowest grade? "))
+
+# if gpa >= .85 and lowest_grade >= .70:
+# 	honour_roll = True
+# else:
+# 	honour_roll = False
+
+# if honour_roll:
+# 	print('You made honour roll.')
+# else:
+# 	print('You did not make honour roll.')
+
+
