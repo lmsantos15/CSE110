2c87df60a6fdb5a3dce8c32d2f6c2287099e5d7e
Laurence Mike Santos <114161105+lmsantos15@users.noreply.github.com>
2022-11-01 15:24:41 +0800

    Update

--- week7/teamact.py
+++ week7/teamact.py
@@ -1,50 +1,33 @@
 # 07 Teach: Team Activity
 # Guess my number game
 
+print()
+
 import random
-print()
 
+number = random.randint(1, 10)
 
-# # Core Requirements
+# Variables
+magic_number = int(input("What is the magic number? "))
+guess_number = int(input("What is your guess? "))
 
-# # 01
-# magic_number = random.randint(1, 10)
-# guess = -1
-
-# if guess != magic_number:
-#     guess = int(input("What is your guess? "))
-
-#     if guess < magic_number:
-#         print("Higher :)")
-#         guess = int(input("What is your guess? "))
-
-#     elif guess > magic_number:
-#         print("Lower :)")
-#         guess = int(input("What is your guess? "))
-
-# else:
-#     print("You guessed it!")
-# I am stock on this part. Was about to use loop, but it says "At this point you won't have any loops."
-# Hoping I understood the instruction correctly.
-print()
-
-# 02
-# Add a loop that keeps looping as long as the guess does not match the magic number.
-
-print()
-
-magic_number = random.randint(1, 10)
-guess = ""
-
-if guess == magic_number:
-    guess = int(input("What is the magic number? "))
-    print("You guessed it!")
-
-    while guess < magic_number:
-        guess = int(input("What is the magic number? "))
+# Looping the questions
+while magic_number != number:
+    if guess_number <= number:
+        print()
         print("Higher")
 
+        magic_number = int(input("What is the magic number? "))
+        guess_number = int(input("What is your guess? "))
+
+    elif guess_number >= magic_number:
+        print()
+        print("Lower")
     
+        magic_number = int(input("What is the magic number? "))
+        guess_number = int(input("What is your guess? "))
 
+print()
 
-
+print("You guess it right!")
+print()
